a7cb2c8955ec61096826cf4aaabedb58577699b9
Benik <galstyan.benik@gmail.com>
2024-06-08 21:46:10 +0300

    setup network service

---
+++ lib/data/service/network_service/dio_network_service_impl.dart
@@ -0,0 +1,53 @@
+import 'package:askm/core/typedefs.dart';
+import 'package:askm/data/service/network_service/network_service.dart';
+import 'package:dio/dio.dart';
+
+class DioNetworkServiceImpl implements NetworkService {
+  DioNetworkServiceImpl(this._dio);
+
+  final Dio _dio;
+
+  @override
+  Future<Response<T>> delete<T>(
+      String url, {
+        required HeaderMap headers,
+      }) =>
+      _dio.delete<T>(
+        url,
+        options: Options(headers: headers),
+      );
+
+  @override
+  Future<Response<T>> get<T>(
+      String url, {
+        required HeaderMap headers,
+      }) =>
+      _dio.get<T>(
+        url,
+        options: Options(headers: headers),
+      );
+
+  @override
+  Future<Response<T>> post<T>(
+      String url, {
+        required HeaderMap headers,
+        required JsonMap body,
+      }) =>
+      _dio.post<T>(
+        url,
+        data: body,
+        options: Options(headers: headers),
+      );
+
+  @override
+  Future<Response<T>> put<T>(
+      String url, {
+        required HeaderMap headers,
+        required JsonMap body,
+      }) =>
+      _dio.put<T>(
+        url,
+        data: body,
+        options: Options(headers: headers),
+      );
+}
---
+++ lib/data/service/network_service/network_service.dart
@@ -0,0 +1,28 @@
+import 'package:askm/core/typedefs.dart';
+import 'package:dio/dio.dart';
+
+typedef HeaderMap = StringMap;
+
+abstract interface class NetworkService {
+  Future<Response<T>> get<T>(
+      String url, {
+        required HeaderMap headers,
+      });
+
+  Future<Response<T>> post<T>(
+      String url, {
+        required HeaderMap headers,
+        required JsonMap body,
+      });
+
+  Future<Response<T>> put<T>(
+      String url, {
+        required HeaderMap headers,
+        required JsonMap body,
+      });
+
+  Future<Response<T>> delete<T>(
+      String url, {
+        required HeaderMap headers,
+      });
+}
