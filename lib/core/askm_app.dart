6fa0f19a24c92c825e5cbc202e93a9655d3efe6d
Benik <galstyan.benik@gmail.com>
2024-06-08 22:47:55 +0300

    setup localization

--- lib/core/askm_app.dart
+++ lib/core/askm_app.dart
@@ -1,5 +1,7 @@
+import 'package:askm/generated/l10n.dart';
 import 'package:askm/presentation/pages/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class AskmApp extends StatefulWidget {
   const AskmApp({super.key});
@@ -17,6 +19,13 @@ class _AskmAppState extends State<AskmApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: const [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       theme: ThemeData.dark(),
       home: const MainScreen(),
       builder: (context, widget) => SafeArea(child: widget ?? const SizedBox()),
