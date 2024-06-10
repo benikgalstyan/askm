3d34897a83e769cc6a4484364891d8dbe9fa02ad
Benik <galstyan.benik@gmail.com>
2024-06-10 11:26:41 +0300

    deleted unused methods

--- lib/data/service/network_service/dio_network_service_impl.dart
+++ lib/data/service/network_service/dio_network_service_impl.dart
@@ -6,48 +6,4 @@ class DioNetworkServiceImpl implements NetworkService {
   DioNetworkServiceImpl(this._dio);
 
   final Dio _dio;
-
-  @override
-  Future<Response<T>> delete<T>(
-      String url, {
-        required HeaderMap headers,
-      }) =>
-      _dio.delete<T>(
-        url,
-        options: Options(headers: headers),
-      );
-
-  @override
-  Future<Response<T>> get<T>(
-      String url, {
-        required HeaderMap headers,
-      }) =>
-      _dio.get<T>(
-        url,
-        options: Options(headers: headers),
-      );
-
-  @override
-  Future<Response<T>> post<T>(
-      String url, {
-        required HeaderMap headers,
-        required JsonMap body,
-      }) =>
-      _dio.post<T>(
-        url,
-        data: body,
-        options: Options(headers: headers),
-      );
-
-  @override
-  Future<Response<T>> put<T>(
-      String url, {
-        required HeaderMap headers,
-        required JsonMap body,
-      }) =>
-      _dio.put<T>(
-        url,
-        data: body,
-        options: Options(headers: headers),
-      );
 }
