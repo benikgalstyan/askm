8d8eaf38d55c08ea849b83063a3b630160e513d4
Benik <galstyan.benik@gmail.com>
2024-06-18 22:28:52 +0300

    added the description parameter for app logo widget

--- lib/presentation/widgets/app_logo.dart
+++ lib/presentation/widgets/app_logo.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:askm/generated/assets/assets.gen.dart';
+import 'package:askm/core/context_extensions.dart';
+import 'package:askm/core/theme/text_styles.dart';
 
 class AppLogo extends StatelessWidget {
-  const AppLogo({super.key});
+  const AppLogo({
+    super.key,
+    this.description,
+  });
+
+  final List<String>? description;
 
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.asset(Assets.images.logo);
+    return Column(
+      children: [
+        SvgPicture.asset(Assets.images.logo),
+        Text(
+          context.s.askm,
+          style: TextStyles.headline1,
+        ),
+        if (description != null)
+          ...description!.map(
+            (text) => Text(text, style: TextStyles.bodyText1),
+          ),
+      ],
+    );
   }
 }
