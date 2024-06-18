82cbb35b17ecd28233e917536ede633f1965a00e
Benik <galstyan.benik@gmail.com>
2024-06-18 16:22:17 +0300

    setup light palette

---
+++ lib/core/theme/palette/light_palette.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LightPalette {
+  static const whiteColor = Colors.white;
+  static const blackColor = Colors.black;
+  static const primaryButtonColor = Color(0xFF177161);
+  static const disabledButtonColor = Color(0xFFB0B0B0);
+  static const secondaryButtonColor = Color(0xFF292727);
+  static const disabledTextButtonColor = Color(0xFF3A3A3A);
+}
