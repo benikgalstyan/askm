b6d4367cd171d882d1a05014a33de045945cbc3d
Benik <galstyan.benik@gmail.com>
2024-06-18 16:23:23 +0300

    formatted the layout

--- lib/presentation/pages/social_sign_up_screen/social_sign_up_layout.dart
+++ lib/presentation/pages/social_sign_up_screen/social_sign_up_layout.dart
@@ -1,10 +1,11 @@
 import 'package:askm/core/context_extensions.dart';
 import 'package:askm/generated/assets/assets.gen.dart';
 import 'package:askm/presentation/tokens/spacing.dart';
+import 'package:askm/presentation/widgets/app_logo.dart';
 import 'package:askm/presentation/widgets/background_widget.dart';
 import 'package:askm/presentation/widgets/custom_button.dart';
+import 'package:askm/presentation/widgets/info_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class SocialSignUpLayout extends StatefulWidget {
   const SocialSignUpLayout({super.key});
@@ -15,50 +16,31 @@ class SocialSignUpLayout extends StatefulWidget {
 
 class _SocialSignUpLayoutState extends State<SocialSignUpLayout> {
   static const upperFlex = 5;
-  final appLogo = SvgPicture.asset(Assets.images.logo);
-  final googleIcon = SvgPicture.asset(Assets.images.googleIcon);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: BackgroundWidget(
+        backgroundImage: Assets.images.backgroundImage.keyName,
         child: Center(
           child: Column(
             children: [
               const Spacer(flex: upperFlex),
-              appLogo,
-              Text(
-                context.s.askm,
-                style: const TextStyle(fontSize: Spacings.xxL),
-              ),
-              Text(
-                context.s.whether,
-                style: const TextStyle(fontSize: Spacings.L),
-              ),
-              Text(
-                context.s.comfort,
-                style: const TextStyle(fontSize: Spacings.L),
-              ),
+              const AppLogo(),
+              const InfoTextsWidget(),
               const SizedBox(height: Spacings.xxxxL),
-              CustomButton(
+              ASKMElevatedButton.secondaryWithIcon(
                 onPressed: () {
                   // TODO(Benik): implement google sign up
                 },
-                icon: googleIcon,
-                buttonColor: Colors.white,
-                textButtonColor: Colors.black,
                 text: context.s.continueWithGoogle,
               ),
-              const SizedBox(
-                height: Spacings.L,
-              ),
-              CustomButton(
+              const SizedBox(height: Spacings.L),
+              ASKMElevatedButton.secondary(
                 onPressed: () {
                   // TODO(Benik): implement email sign up
                 },
-                textButtonColor: Colors.white,
                 text: context.s.continueWithEmail,
-                buttonColor: const Color(0xFF292727),
               ),
               const Spacer(),
             ],
