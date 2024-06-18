477ca859bc3a020fa27041e044d05dfbe4552be7
Benik <galstyan.benik@gmail.com>
2024-06-18 16:19:56 +0300

    added parameter background image

--- lib/presentation/widgets/background_widget.dart
+++ lib/presentation/widgets/background_widget.dart
@@ -1,23 +1,24 @@
-import 'package:askm/generated/assets/assets.gen.dart';
 import 'package:flutter/material.dart';
 
 class BackgroundWidget extends StatelessWidget {
   const BackgroundWidget({
     super.key,
     required this.child,
+    required this.backgroundImage,
   });
 
   final Widget child;
+  final String backgroundImage;
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
         Image.asset(
+          backgroundImage,
           fit: BoxFit.cover,
           width: double.infinity,
           height: double.infinity,
-          Assets.images.backgroundImage.keyName,
         ),
         child,
       ],
