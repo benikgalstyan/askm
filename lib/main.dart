d28b2183d9a26a44cc99934cba46eafb3c6bedea
Benik <galstyan.benik@gmail.com>
2024-06-20 15:48:46 +0300

    firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:askm/askm_app.dart';
 import 'package:askm/service_locator.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   setupServicesLocator();
   runApp(const AskmApp());
 }
