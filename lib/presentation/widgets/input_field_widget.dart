f9e1e3d37e845c0fbf29f9a607f2a1d387f8ea3c
Benik <galstyan.benik@gmail.com>
2024-07-13 19:13:05 +0300

    update

--- lib/presentation/widgets/input_field_widget.dart
+++ lib/presentation/widgets/input_field_widget.dart
@@ -6,58 +6,54 @@ import 'package:askm/core/theme/text_styles.dart';
 class InputFieldWidget extends StatelessWidget {
   InputFieldWidget({
     super.key,
-    this.textStyle,
     this.validator,
     this.controller,
-    this.decoration,
     required this.labelText,
   });
 
   final String labelText;
-  final TextStyle? textStyle;
-  final InputDecoration? decoration;
   final TextEditingController? controller;
   final SingleRegistrationValidatorFunction? validator;
 
   final defaultBorderRadius = BorderRadius.circular(10);
+  static const contentPadding = EdgeInsets.symmetric(vertical: 14, horizontal: 14);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      style: TextStyles.inputText1,
-      validator: validator,
-      decoration: decoration ??
-          InputDecoration(
-            contentPadding: const EdgeInsets.symmetric(vertical: 14, horizontal: 14),
-            errorStyle: TextStyles.errorText1,
-            labelText: labelText,
-            labelStyle: TextStyles.labelText1,
-            floatingLabelBehavior: FloatingLabelBehavior.auto,
-            floatingLabelStyle: TextStyles.labelText2,
-            focusedBorder: OutlineInputBorder(
-              borderSide: const BorderSide(color: LightPalette.focusColor),
-              borderRadius: defaultBorderRadius,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: const BorderSide(color: LightPalette.borderColor),
-              borderRadius: defaultBorderRadius,
-            ),
-            errorBorder: OutlineInputBorder(
-              borderSide: const BorderSide(color: LightPalette.errorColor),
-              borderRadius: defaultBorderRadius,
-            ),
-            focusedErrorBorder: OutlineInputBorder(
-              borderSide: const BorderSide(color: LightPalette.errorColor),
-              borderRadius: defaultBorderRadius,
-            ),
-            border: OutlineInputBorder(
-              borderSide: const BorderSide(color: LightPalette.borderColor),
-              borderRadius: defaultBorderRadius,
-            ),
-          ),
-      controller: controller,
       obscureText: false,
+      validator: validator,
+      controller: controller,
+      style: TextStyles.inputText1,
       cursorColor: LightPalette.cursorColor,
+      decoration: InputDecoration(
+        labelText: labelText,
+        contentPadding: contentPadding,
+        errorStyle: TextStyles.errorText1,
+        labelStyle: TextStyles.labelText1,
+        floatingLabelStyle: TextStyles.labelText2,
+        floatingLabelBehavior: FloatingLabelBehavior.auto,
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: LightPalette.focusColor),
+          borderRadius: defaultBorderRadius,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: LightPalette.borderColor),
+          borderRadius: defaultBorderRadius,
+        ),
+        errorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: LightPalette.errorColor),
+          borderRadius: defaultBorderRadius,
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: LightPalette.errorColor),
+          borderRadius: defaultBorderRadius,
+        ),
+        border: OutlineInputBorder(
+          borderSide: const BorderSide(color: LightPalette.borderColor),
+          borderRadius: defaultBorderRadius,
+        ),
+      ),
     );
   }
 }
