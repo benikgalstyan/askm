81322fb40bf145328b879ef40e6357d6f2738cd5
Benik <galstyan.benik@gmail.com>
2024-08-31 14:37:26 -0700

    updated sign up result model

--- lib/data/models/sign_up_result.dart
+++ lib/data/models/sign_up_result.dart
@@ -2,8 +2,11 @@ import 'package:askm/data/models/errors/sign_up_exception.dart';
 import 'package:askm/data/models/user.dart';
 
 class SignUpResult {
-  SignUpResult(this.user, this.error, this.token);
+  SignUpResult(this.user, this.error, this.accessToken);
+
   final User? user;
-  final String? token;
   final SignUpException? error;
+  final String? accessToken;
+
+  bool get success => user != null && error == null;
 }
