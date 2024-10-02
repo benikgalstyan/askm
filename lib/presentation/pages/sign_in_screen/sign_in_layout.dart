2069293eb34bc2cfe810ec17b5b41f62fc7c585f
Benik <galstyan.benik@gmail.com>
2024-10-02 06:19:59 +0300

    implemented sign in screen

---
+++ lib/presentation/pages/sign_in_screen/sign_in_layout.dart
@@ -0,0 +1,54 @@
+import 'package:askm/core/router/router.dart';
+import 'package:askm/presentation/pages/sign_up_screen/provider/auth_controller.dart';
+import 'package:askm/presentation/pages/sign_up_screen/provider/button_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:askm/core/context_extensions.dart';
+import 'package:askm/core/theme/text_styles.dart';
+import 'package:askm/presentation/tokens/spacing.dart';
+import 'package:askm/presentation/widgets/sign_up_form.dart';
+
+class SignInLayout extends ConsumerWidget {
+  const SignInLayout({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isButtonEnabled = ref.watch(isSignUpButtonEnabledProvider);
+
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+          padding: Spacings.paddingH20,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Spacings.spacer64,
+              Text(context.s.signIn, style: TextStyles.headline2),
+              Spacings.spacer32,
+              SignUpForm(
+                onSignInButtonPressed: (String email, String password) async {
+                  if (isButtonEnabled) {
+                    final success = await ref
+                        .read(authControllerProvider.notifier)
+                        .signIn(email, password);
+
+                    if (success) {
+                      await context.r.replaceAll([MainRoute()]);
+                    } else {
+                      // TODO(Benik): Implement error screen
+                      const Text('Error');
+                    }
+                  }
+                },
+                isSignUp: false,
+              ),
+              Spacings.spacer48,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
