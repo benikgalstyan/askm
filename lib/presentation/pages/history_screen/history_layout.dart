66a245cfbea4fe225befc9723ad30cbb2abd6584
Benik <galstyan.benik@gmail.com>
2024-10-24 02:29:46 +0300

    implemented history shimmer

--- lib/presentation/pages/history_screen/history_layout.dart
+++ lib/presentation/pages/history_screen/history_layout.dart
@@ -7,106 +7,142 @@ import 'package:askm/presentation/pages/sign_up_screen/provider/auth_controller.
 import 'package:askm/presentation/tokens/spacing.dart';
 import 'package:askm/presentation/widgets/dialog_manager_utils.dart';
 import 'package:askm/presentation/widgets/history_app_bar.dart';
+import 'package:askm/presentation/widgets/history_screen_shimmer.dart';
 import 'package:askm/presentation/widgets/session_container.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 
-class HistoryLayout extends ConsumerWidget {
+class HistoryLayout extends ConsumerStatefulWidget {
   const HistoryLayout({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    const iconSize = 32.0;
-    final firestoreController =
-        ref.watch(chatSessionControllerProvider.notifier);
-    const logoutIcon = Icon(Icons.logout, size: iconSize);
+  ConsumerState<HistoryLayout> createState() => _HistoryLayoutState();
+}
 
-    return Scaffold(
-      appBar: HistoryAppBarWidget(
-        onHistoryCloseTap: () => context.r.maybePop(),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: FutureBuilder(
-              future: firestoreController.loadChatSessions(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else if (snapshot.data!.isEmpty) {
-                  return Center(child: Text(context.s.noChatSessionsFound));
-                } else {
-                  final chatSessions = snapshot.data!;
-                  chatSessions
-                      .sort((a, b) => b.createdAt.compareTo(a.createdAt));
-                  final groupedSessions = _groupSessionsByTime(chatSessions);
-                  return Padding(
-                    padding: Spacings.paddingH16,
-                    child: ListView.builder(
-                      itemCount: groupedSessions.length,
-                      itemBuilder: (context, index) {
-                        final dateGroup = groupedSessions[index];
-                        return Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Padding(
-                              padding: Spacings.paddingH16,
-                              child: Text(
-                                dateGroup['title']!,
-                                style: TextStyles.historyTitle,
-                              ),
+class _HistoryLayoutState extends ConsumerState<HistoryLayout> {
+  late Future<List<ChatSession>> _chatSessionsFuture;
+
+  bool isLoading = true;
+  static const iconSize = 32.0;
+  static bool _hasShownShimmer = false;
+  static const logoutIcon = Icon(Icons.logout, size: iconSize);
+
+  @override
+  void initState() {
+    super.initState();
+    _chatSessionsFuture =
+        ref.read(chatSessionControllerProvider.notifier).loadChatSessions();
+    _startLoading();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar:
+            HistoryAppBarWidget(onHistoryCloseTap: () => context.r.maybePop()),
+        body: isLoading
+            ? HistoryScreenShimmer()
+            : Column(
+                children: [
+                  Expanded(
+                    child: FutureBuilder(
+                      future: _chatSessionsFuture,
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        } else if (snapshot.hasError) {
+                          return Center(
+                            child: Text('Error: ${snapshot.error}'),
+                          );
+                        } else if (snapshot.data!.isEmpty) {
+                          return Center(
+                            child: Text(context.s.noChatSessionsFound),
+                          );
+                        } else {
+                          final chatSessions = snapshot.data!;
+                          chatSessions.sort(
+                            (a, b) => b.createdAt.compareTo(a.createdAt),
+                          );
+                          final groupedSessions =
+                              _groupSessionsByTime(chatSessions);
+                          return Padding(
+                            padding: Spacings.paddingH16,
+                            child: ListView.builder(
+                              itemCount: groupedSessions.length,
+                              itemBuilder: (context, index) {
+                                final dateGroup = groupedSessions[index];
+                                return Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Padding(
+                                      padding: Spacings.paddingH16,
+                                      child: Text(
+                                        dateGroup['title']!,
+                                        style: TextStyles.historyTitle,
+                                      ),
+                                    ),
+                                    Spacings.spacer8,
+                                    ...((dateGroup['sessions']
+                                            as List<ChatSession>)
+                                        .map(
+                                      (session) => SessionsContainer(
+                                        chatSession: session,
+                                      ),
+                                    )),
+                                    if (index < groupedSessions.length - 1)
+                                      const Padding(
+                                        padding: Spacings.paddingH12,
+                                        child: Divider(),
+                                      ),
+                                  ],
+                                );
+                              },
                             ),
-                            Spacings.spacer8,
-                            ...((dateGroup['sessions'] as List<ChatSession>)
-                                .map(
-                              (session) =>
-                                  SessionsContainer(chatSession: session),
-                            )),
-                            if (index < groupedSessions.length - 1)
-                              const Padding(
-                                padding: Spacings.paddingH12,
-                                child: Divider(),
-                              ),
-                          ],
-                        );
+                          );
+                        }
                       },
                     ),
-                  );
-                }
-              },
-            ),
-          ),
-          Padding(
-            padding: Spacings.padding16,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  icon: logoutIcon,
-                  onPressed: () => AlertManagerUtils.showSignOutWarning(
-                    context,
-                    onCancel: () => context.r.maybePop(),
-                    onSubmit: () async {
-                      await ref.read(authControllerProvider.notifier).logout();
-                      await context.r.replaceAll([const SocialSignUpRoute()]);
-                    },
                   ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+                  Padding(
+                    padding: Spacings.padding16,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          icon: logoutIcon,
+                          onPressed: () => AlertManagerUtils.showSignOutWarning(
+                            context,
+                            onCancel: () => context.r.maybePop(),
+                            onSubmit: () async {
+                              await ref
+                                  .read(authControllerProvider.notifier)
+                                  .logout();
+                              await context.r
+                                  .replaceAll([const SocialSignUpRoute()]);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+      );
+
+  // TODO(benik): rewrite later
+  Future<void> _startLoading() async {
+    if (!_hasShownShimmer) {
+      await Future.delayed(const Duration(seconds: 2));
+      _hasShownShimmer = true;
+    }
+    setState(() => isLoading = false);
   }
 
   List<Map<String, dynamic>> _groupSessionsByTime(List<ChatSession> sessions) {
     final currentTime = DateTime.now();
-    final List<Map<String, dynamic>> groupedSessions = [];
-
     final Map<String, List<ChatSession>> groups = {
       'Today': [],
       'Yesterday': [],
@@ -134,22 +170,18 @@ class HistoryLayout extends ConsumerWidget {
         groups['Last 30 Days']!.add(session);
       } else {
         final monthName = DateFormat('MMMM').format(session.createdAt);
-        if (!groups.containsKey(monthName)) {
-          groups[monthName] = [];
-        }
-        groups[monthName]!.add(session);
+        groups.putIfAbsent(monthName, () => []).add(session);
       }
     }
 
-    for (var entry in groups.entries) {
-      if (entry.value.isNotEmpty) {
-        groupedSessions.add({
-          'title': entry.key,
-          'sessions': entry.value,
-        });
-      }
-    }
-
-    return groupedSessions;
+    return groups.entries
+        .where((entry) => entry.value.isNotEmpty)
+        .map(
+          (entry) => {
+            'title': entry.key,
+            'sessions': entry.value,
+          },
+        )
+        .toList();
   }
 }
